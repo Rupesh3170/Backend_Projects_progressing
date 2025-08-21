2f570a541a1db00b9dfee10ac6314954f013375c
Rupesh3170 <rupeshkumarsah3170@gmail.com>
2025-08-22 01:25:04 +0530

    fix: MongoDB connection with dotenv config

---
+++ src/index.js
@@ -0,0 +1,80 @@
+//require('dotenv').config({path:'./env'})    --it is also fine 
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* methods for connection of databse in the same file 
+
+import express from "express"
+const app=express()
+
+// function connectDB(){}           method 1-> for connecting db
+// connectDB()
+
+//best way-->
+;(async()=>{
+
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+       //listners  (it is the part of express)
+       app.on("error",(error)=>{
+        console.log("app not able to talk to database");
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on port ${process.env.PORT}`)
+       })
+
+
+    }catch(error){
+        console.log("ERROR:",error)
+        throw err
+
+    }
+
+})()  */
