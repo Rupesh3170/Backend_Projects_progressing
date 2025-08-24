e572df77736b32a590105c73ccd1300d36876c0c
Rupesh3170 <rupeshkumarsah3170@gmail.com>
2025-08-24 17:19:01 +0530

    update

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,50 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//try catch using m2->
+// const asyncHandler=()=>{}
+// const asyncHandler=(fun)=>()=>{}
+// const asyncHandler=(fun)=>async()=>{}
+
+
+
+// const asyncHandler =(fn)=>async (req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+
+//     }catch(error){
+//         res.status(error.code||500).json({
+//             sucess:false,
+//             message:err.message
+//         })
+
+//     }
+
+
+// }
