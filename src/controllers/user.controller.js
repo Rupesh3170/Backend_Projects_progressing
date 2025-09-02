6348c7c02379a47bff92d73ffc50b151baeaf3e0
Rupesh3170 <rupeshkumarsah3170@gmail.com>
2025-09-02 23:08:48 +0530

    little changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,8 @@ const registerUser=asyncHandler(async(req,res)=>{
 
 
     const {fullname,email,username,password}=req.body
-    console.log("email: ",email);
+
+    //console.log("email: ",email);
 
     // if(fullName===""){
     //     throw new ApiError(400,"fullname is required")
@@ -50,7 +51,15 @@ const registerUser=asyncHandler(async(req,res)=>{
     }
 
    const avatarLocalPath= req.files?.avatar[0]?.path;
-   const coverImageLocalPath=req.files?.coverImage[0]?.path;
+//    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+          coverImageLocalPath=req.files.coverImage[0].path
+    }
+
+
 
    if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
@@ -91,6 +100,8 @@ return res.status(201).json(
 
 
 })
+
+
 
 
 
